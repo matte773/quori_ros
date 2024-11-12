fa0af9a63c09b06bc238dc03a93a62948fbe0bdf
Matt M <matte773@gmail.com>
2024-11-11 18:17:52 -0800

    fixing face calibration

--- src/quori_osu/src/face_calibration.py
+++ src/quori_osu/src/face_calibration.py
@@ -1,29 +1,145 @@
 #!/usr/bin/env python3
 
-import sys
+import os, sys
 import numpy as np
 import cv2
 from PIL import Image
-import 
+from itertools import count
 
 
-def move_selected_pixels():
-    pass
+class Eye:
+    """dataclass for representing a segment of the image."""
+    def  __init__(self, circle, img_shape, max_tol=2, debug=True):
+        circle = np.uint16(np.around(circle)) # round 
+        self.center = np.array((circle[0], circle[1]))
+        self.radius = circle[2]
+        squaring = np.array([self.radius, self.radius])
+        self.corners = [self.center - squaring, self.center+squaring]
+        #make sure tol doesn't exceed size of img 
+        min_index = min(self.corners[0])
+        tol = min(min_index, max_tol)
+        tol = min(tol, min(img_shape - self.corners[1] -1))
+        self.corners[0] -= tol
+        self.corners[1] += tol
+
+        if debug:
+            print("-----Eye Debug-----")
+            print(f"Center: {self.center}")
+            print(f"Radius: {self.radius}")
+            print(f"Image Shape: {img_shape}")
+            print(f"Box Size: {self.corners[1] - self.corners[0]}")
+            print("-------------------")
+
+def get_index_error(p1,p2,outer_shape):
+    """
+    Uses p1 to make sure we don't clip into negatives and p2 to make sure we don't exceed 
+    subtract this from p1 and p2 to produce valid indices.
+    """
+    minimums = np.where(p1 < 0, p1, 0) # handles negative indices
+    maximums = np.where(p2 - outer_shape + 1 > 0, p2, 0)
+    return minimums+maximums
+    
+
+def move_selected_pixels(image, bounding_box, dx, dy):
+    """
+    Move a selection of the image, handles edge cases poorly
+    """
+    p1, p2 = bounding_box
+    selection = image[p1[1]:p2[1], p1[0]:p2[0]].copy()
+    image[p1[1]:p2[1], p1[0]:p2[0]] = np.mean(image)
+    p1_prime = p1 + np.array((dx, dy))
+    p2_prime = p2 + np.array((dx, dy))
+    bounding_error = get_index_error(p1_prime, p2_prime, image.shape[0:2])
+    print(p1_prime)
+    print(p2_prime)
+    print(bounding_error)
+    p1_prime -= bounding_error
+    p2_prime -= bounding_error
+    image[p1_prime[1]:p2_prime[1], p1_prime[0]:p2_prime[0]] = selection
+    return p1_prime, p2_prime
+
+
+def stretch_selected_pixels(image, bounding_box, sx, sy):
+    """
+    Scale a selection of the image, doesn't handle edge cases
+    """
+    p1, p2 = bounding_box
+    selection = cv2.resize(image[p1[1]:p2[1], p1[0]:p2[0]], (0,0), fx=sx, fy=sy)
+    out_shape = np.array(selection.shape[0:2])
+    image[p1[1]:p2[1], p1[0]:p2[0]] = np.mean(image)
+    center = (p1 + p2) / 2
+    p1_prime = np.uint16(center - out_shape // 2)
+    p2_prime = np.uint16(center + (out_shape - out_shape // 2)) # handles odd numbers
+    
+    image[p1_prime[1]:p2_prime[1], p1_prime[0]:p2_prime[0]] = selection
+    return p1_prime, p2_prime
+
+
+def shift_eyes(image, eyes, ops):
+    image = image.convert('RGB')
+    pixels = np.array(image).astype(np.uint16)
+    output = pixels.copy()
+    for i in range(len(eyes)):
+        op = ops[str(i)]
+        eyes[i].corners = move_selected_pixels(output, eyes[i].corners, dx=op['dx'], dy=op['dy'])
+        eyes[i].corners = stretch_selected_pixels(output, eyes[i].corners, sx=op['sx'], sy=op['sy'])
+    return Image.fromarray(np.uint8(output))
 
 
 if __name__ == "__main__":
+    
+    operations = {
+        '0': { # First Eye
+            'dx': 10,
+            'dy': 20,
+            'sx': 1.1,
+            'sy': 1.1, 
+        },
+        '1': { # Second Eye
+            'dx': 0,
+            'dy': 0,
+            'sx': 1,
+            'sy': 1, 
+        }
+    }
     try:
-        im = Image.open(sys.argv[1])
+        filepath = sys.argv[1]
+        im = Image.open(filepath)
+        im = im.convert('RGB')
+        pixels = np.array(im)
+        pixels = pixels.astype(np.uint8)
+        output = pixels.copy()
+
     except Exception as e:
-        print(f"Usage: python3 {sys.argv[0]} <Image_Filename>")
+        print(f"Usage: python3 {sys.argv[0]} <Image_Filename> [transform_list_yaml]")
         raise e
 
 
-    print(np.array(im))
-    cv2.imshow("window", np.array(im))
-    cv2.waitKey(0)
-    # gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    # _,binary = cv2.threshold(gray,100,255,cv2.THRESH_BINARY)
-    # image, contours, h = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.drawContours(image,contours, -1, (0,255,0),3)
+    gray = cv2.cvtColor(pixels, cv2.COLOR_RGB2GRAY)
+    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,20,
+                            param1=50,param2=30,minRadius=60,maxRadius=0)
+ 
+    if len(circles[0]) != 2:
+        print(f"Did not detect two eyes, instead found: {circles}")
+        exit(1)
     
+    # hold the eyes ready for processing
+    eyes = [Eye(c, gray.shape) for c in circles[0]]
+
+    images = []
+    delays = []
+    try:
+        for i in count(1):
+            images.append(
+                shift_eyes(im, eyes, operations)
+                )
+            try:
+                delays.append(im.info['duration'])
+            except:
+                delays.append(100)
+            im.seek(i)
+    except EOFError:
+        pass
+
+    filename = filepath.split(os.sep)[-1]
+    images[0].save(os.path.join('src/faces', filename), save_all=True, append_images=images[1:], duration=delays)
